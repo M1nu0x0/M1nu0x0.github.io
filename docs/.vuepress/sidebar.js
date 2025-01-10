5874acf9c5b38831284057cbbb0de9601f99240e
M1nu0x0 <oxygen3112@daum.net>
2025-01-10 17:01:22 +0900

    중간 저장

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -37,7 +37,7 @@ export const sidebar = {
             "/posts/Languages/Python/02. Python 기본 문법.md",
             "/posts/Languages/Python/03. Python 객체 모델.md",
             "/posts/Languages/Python/04. 클래스와 객체 지향.md",
-            "/posts/Languages/Python/05. Iterable. Iterator, Generator.md",
+            "/posts/Languages/Python/05. Iterable_Iterator_Generator.md",
             "/posts/Languages/Python/06. argparse와 스크립트 실행.md",
             "/posts/Languages/Python/07. 라이브러리 작성과 관리.md",
             "/posts/Languages/Python/08. 고급 Python 주제.md",
